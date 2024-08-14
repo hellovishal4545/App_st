c547b0fb84c46c6a98b2cb37a1785f23f7e408ef
hellovishal4545 <165807760+hellovishal4545@users.noreply.github.com>
2024-08-14 19:34:16 +0530

    Add files via upload

--- us_stocks.py
+++ us_stocks.py
@@ -1,5 +1,15 @@
-import streamlit as st, pandas as pd, numpy as np , yfinance as yf
+import streamlit as st
+import pandas as pd
+import numpy as np 
+import yfinance as yf
 import plotly.express as px 
+#from timeseries import TimeSeries 
+#from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.fundamentaldata import FundamentalData
+from stocknews import StockNews  
+import streamlit.components.v1 as com
+#from streamlit_lottie import st_lottie
+com.iframe("https://lottie.host/embed/5cfc1a3d-76ad-400b-9db0-023947afae57/lvgGWjs49p.json")
 
 st.title("Stock Dashboard")
 ticker = st.sidebar.text_input("Ticker",value = 'AAPL')
@@ -24,12 +34,10 @@ with pricing_data:
     stdev = np.std(data2['% Change'])*np.sqrt(252)
     st.write('Standard Deviation is',stdev*100,'%')
     st.write('Risk Adj. Return is',annual_return/(stdev*100))
-    
-from alpha_vantage.timeseries import TimeSeries
-from alpha_vantage.fundamentaldata import FundamentalData
+        
 
 with fundamental_data:
-    key = 'OW1639L6385UCYYL'
+    key = '6XRX4F4BGVMDPLAQ'    #'OW1639L6385UCYYL'
     fd= FundamentalData(key,output_format = 'pandas')
     st.subheader('Balance Sheet')
     balance_sheet = fd.get_balance_sheet_annual(ticker)[0]
@@ -45,12 +53,8 @@ with fundamental_data:
     cash_flow = fd.get_cash_flow_annual(ticker)[0]
     cf = cash_flow.T[2:]
     cf.columns = list(cash_flow.T.iloc[0])
-    st.write(cf)
-    
-        
-    
-       
-from stocknews import StockNews  
+    st.write(cf)      
+
 with news:
     st.header(f'Nes of {ticker}')
     sn = StockNews(ticker,save_news = False)
